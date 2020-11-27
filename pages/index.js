56d91f903b750136df1ac2eff65c96e3d5e945df
Mark Hendrickson <markhendrickson@Mac-mini.local>
2020-11-27 17:11:31 +0100

    Initiate app

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,79 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { showBlockstackConnect } from '@stacks/connect';
+import { UserSession, AppConfig } from '@stacks/auth';
+import { openContractCall } from '@stacks/connect';
+
+const appConfig = new AppConfig()
+const userSession = new UserSession({ appConfig })
+
+let appName = "Bimba Coin"
+let imagePath = '/images/bimba.jpeg'
 
 export default function Home() {
+  function authenticate() {
+    showBlockstackConnect({
+      redirectTo: '/',
+      finished: ({ userSession }) => {
+        console.log('yay!');
+      },
+      appDetails: {
+        name: appName,
+        icon: window.location.origin + imagePath
+      }
+    })
+  }
+
+  function isUserSignedIn() {
+    console.log(userSession.isUserSignedIn());
+  }
+
+  function getUserData() {
+    console.log(userSession.loadUserData());
+  }
+
+  function callContract() {
+    const myStatus = 'hey there';
+    const options = {
+      contractAddress: 'ST22T6ZS7HVWEMZHHFK77H4GTNDTWNPQAX8WZAKHJ',
+      contractName: 'status',
+      functionName: 'write-status!',
+      functionArgs: [
+        {
+          type: 'buff',
+          value: myStatus,
+        },
+      ],
+      appDetails: {
+        name: appName,
+        icon: window.location.origin + imagePath
+      },
+      finished: data => {
+        console.log('TX ID:', data.txId);
+        console.log('Raw TX:', data.txRaw);
+      },
+    };
+
+    openContractCall(options);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{appName}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
+        <img src={imagePath} className={styles.photo} />
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {appName}
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <button className={styles.button} onClick={authenticate}>authenticate</button>
+        <button className={styles.button} onClick={isUserSignedIn}>isUserSignedIn</button>
+        <button className={styles.button} onClick={getUserData}>getUserData</button>
+        <button className={styles.button} onClick={callContract}>openContractCall</button>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
